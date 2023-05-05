4572ae9608130a94528ee33194749932fe0ed986
DavideAtzo <atzo9477@gmail.com>
2023-05-05 18:26:17 +0200

    non mi prende l'id del treno che partono oggi

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use App\Models\Train;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -11,6 +12,8 @@ class PageController extends Controller
     public function index(){
 
         $trains = Train::all();
-        return view('home', compact('trains'));
+        $train = Train::whereDate('departure', Carbon::today())->get();
+        // dd($train);
+        return view('home', compact('trains','train'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,33 +1,52 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-         @vite('resources/js/app.js')
-    </head>
-    <body>
-        <div class="container">
-            <div class="row">
-                <div class="my-2">
-                    <h1>Movies</h1>
-                </div>
-                <ul class="list-unstyled d-flex justify-content-between flex-wrap gap-2">
-                    @foreach ($trains as $train)
-                        <li class="card col-3">
-                            <div class="card-body">
-                                <h5>Compagnia: {{$train->train_company}}</h5>
-                                <p>Codice treno: {{$train->id_train}}</p>
-                                <p>Stazione di partenza: {{$train->departure_station}}</p>
-                                <p>Stazione di arrivo: {{$train->arrived_station}}</p>
-                                <p>Partenza: {{$train->departure}}</p>
-                                <p>Arrivo: {{$train->arrivals}}</p>
-                                <p>N°carrozze: {{$train->n_carriages}}</p>
-                            </div>
-                        </li>
-                    @endforeach
-                </ul>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="my-2">
+                <h1>Treni</h1>
             </div>
+            <ul class="list-unstyled d-flex justify-content-between flex-wrap gap-2">
+                @foreach ($trains as $train)
+                    <li class="card col-3">
+                        <div class="card-body">
+                            <h5>Compagnia: {{ $train->train_company }}</h5>
+                            <p>Codice treno: {{ $train->id_train }}</p>
+                            <p>Stazione di partenza: {{ $train->departure_station }}</p>
+                            <p>Stazione di arrivo: {{ $train->arrived_station }}</p>
+                            <p>Partenza: {{ $train->departure }}</p>
+                            <p>Arrivo: {{ $train->arrivals }}</p>
+                            <p>N°carrozze: {{ $train->n_carriages }}</p>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
         </div>
-    </body>
+    </div>
+    {{-- treni disponibili oggi --}}
+</body>
+<div class="container">
+    <div class="row">
+        <div class="my-2">
+            <h1>Treni di oggi</h1>
+        </div>
+        <ul class="list-unstyled d-flex justify-content-between flex-wrap gap-2">
+            @foreach ($train as $now_train)
+                <li class="card col-3">
+                    <div class="card-body">
+                        <h5>Compagnia: {{ $now_train->train_company }}</h5>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+</div>
 </html>
